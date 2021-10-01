6798f66725bf6ef23a69ee3bb5e33c3705b263f8
Logan Jensen <logan.neil.jensen@gmail.com>
2021-10-01 15:46:14 -0500

    Implemented checking resources

--- Day 15 - Coffee Maker/main.py
+++ Day 15 - Coffee Maker/main.py
@@ -34,12 +34,32 @@ resources = {
 def print_report():
     print('Here is the current report for the coffee machine:')
     for key, value in resources.items():
-        print(key, ' ', value)
+        if key == 'water' or key == 'milk':
+            print(f'{value} mL')
+        elif key == 'coffee':
+            print(f'{value} g')
 
+def check_resources(selection):
+    drink_chosen = MENU[selection]
+
+    for ingredient, amount_needed in drink_chosen['ingredients'].items():
+        curr_resource_value = get(resources[ingredient])
+        if amount_needed > curr_resource_value:
+            print(f'Error. Not enough {ingredient} remaining. Make another selection.')
 
 user_choice = ''
 while user_choice != 'Off':
     user_choice = input('What would you like? (espresso/latte/cappuccino): ')
 
-    if user_choice == 'report':
+    # Verify user chose correct option; re-prompt user if not
+    if user_choice not in ('espresso', 'latte', 'cappuccino', 'Report', 'Off'):
+        print('Invalid choice. Please choose espresso, latte, or cappuccino.')
+        print('You can also exit by choosing \'Off\'')
+    elif user_choice == 'Report':
         print_report()
+    elif user_choice == 'Off':
+        break
+    else:
+        check_resources(user_choice)
+
+print('Thanks for using the coffee maker program!')
