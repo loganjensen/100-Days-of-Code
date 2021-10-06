9c50dd5957518655c3f31ab52890fa712b03ecee
Logan Jensen <logan.neil.jensen@gmail.com>
2021-10-06 16:07:16 -0500

    Create different colored polygons with Turtle

--- Day 18 - Turtle Graphics/main.py
+++ Day 18 - Turtle Graphics/main.py
@@ -1,12 +1,15 @@
 from turtle import Turtle, Screen
+import random
 
+colors = ["white smoke", "dark green", "olive drab", "dark red", "orange", "indigo", "indian red", "dark cyan"]
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.penup()
 timmy.goto(-50, 200)
 timmy.pendown()
 
-for i in range(3, 15):
+for i in range(3, 16):
+    timmy.color(random.choice(colors))
     for j in range(i):
         timmy.pendown()
         timmy.forward(100)
