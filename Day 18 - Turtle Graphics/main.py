7dbccd35a590baf7950d011ab9ec81999e0b0ce2
Logan Jensen <logan.neil.jensen@gmail.com>
2021-10-07 08:42:04 -0500

    Spirograph drawing completed

--- Day 18 - Turtle Graphics/main.py
+++ Day 18 - Turtle Graphics/main.py
@@ -1,20 +1,49 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-colors = ["white smoke", "dark green", "olive drab", "dark red", "orange", "indigo", "indian red", "dark cyan"]
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.penup()
-timmy.goto(-50, 200)
-timmy.pendown()
 
-for i in range(3, 16):
-    timmy.color(random.choice(colors))
-    for j in range(i):
-        timmy.pendown()
-        timmy.forward(100)
-        timmy.right(360/i)
+colors = ["dark green", "olive drab", "dark red", "orange", "indigo", "indian red", "dark cyan", "magenta", "navy", "powder blue"]
+directions = [0, 90, 180, 270]
+
+timmy = t.Turtle()
+t.colormode(255)
+timmy.pensize(1)
+timmy.speed("fastest")
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 
 
-screen = Screen()
+#
+# DRAW POLYGONS
+#
+# for i in range(3, 16):
+#     timmy.color(random.choice(colors))
+#     for j in range(i):
+#         timmy.pendown()
+#         timmy.forward(100)
+#         timmy.right(360/i)
+
+#
+# RANDOM WALK
+#
+# for _ in range(250):
+#     timmy.color(random_color())
+#     timmy.forward(20)
+#     timmy.right(random.choice(directions))
+
+def draw_spirograph(size_of_gap):
+    for i in range(int(360 / size_of_gap)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        current_heading = timmy.heading()
+        timmy.setheading(current_heading + size_of_gap)
+
+draw_spirograph(1)
+
+screen = t.Screen()
 screen.exitonclick()
