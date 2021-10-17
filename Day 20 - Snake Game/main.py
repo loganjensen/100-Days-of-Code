7c4bc72f00649297264e508de7b0192961296fd4
Logan Jensen <logan.neil.jensen@gmail.com>
2021-10-17 11:57:41 -0500

    Implement snake class

--- Day 20 - Snake Game/main.py
+++ Day 20 - Snake Game/main.py
@@ -10,13 +10,17 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
+    time.sleep(0.2)
 
     snake.move()
-
-
 
 screen.exitonclick()
