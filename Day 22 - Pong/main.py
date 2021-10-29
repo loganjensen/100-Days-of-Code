12a5950d9abf56146c415d71d76fb460d72d3d8e
Logan Jensen <logan.neil.jensen@gmail.com>
2021-10-28 19:48:36 -0500

    Implement one paddle

---
+++ Day 22 - Pong/main.py
@@ -0,0 +1,38 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.speed("fastest")
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkeypress(go_up, "Up")
+screen.onkeypress(go_down, "Down")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+screen.exitonclick()
