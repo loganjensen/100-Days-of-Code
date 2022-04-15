54ef9beeef5a210a6866f9bfd3dd7217fbb4c5d4
Logan Jensen <logan.neil.jensen@gmail.com>
2022-04-14 19:13:06 -0500

    Implement ball

--- Day 22 - Pong/ball.py
+++ Day 22 - Pong/ball.py
@@ -8,8 +8,16 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+        self.xmove = 10
+        self.ymove = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.xmove
+        new_y = self.ycor() + self.ymove
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.ymove *= -1
+
+    def bounce_x(self):
+        self.xmove *= -1
--- Day 22 - Pong/main.py
+++ Day 22 - Pong/main.py
@@ -26,4 +26,16 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # Detect collision with wall
+    if ball.ycor() > 275 or ball.ycor() < -275:
+        ball.bounce_y()
+
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    # Detect collision with left paddle
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
 screen.exitonclick()
