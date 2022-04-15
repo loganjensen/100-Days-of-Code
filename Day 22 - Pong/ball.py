ea822ace9d6cf2db1cd8c2d9a1ed3296f4acf88e
Logan Jensen <logan.neil.jensen@gmail.com>
2022-04-15 12:07:47 -0500

    Scoreboard implemented

--- Day 22 - Pong/ball.py
+++ Day 22 - Pong/ball.py
@@ -21,3 +21,7 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.xmove *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
--- Day 22 - Pong/main.py
+++ Day 22 - Pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
+speed = 0.2
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,7 +25,7 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(speed)
     screen.update()
     ball.move()
 
@@ -33,9 +36,23 @@ while game_is_on:
     # Detect collision with right paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
         ball.bounce_x()
+        if speed >= 0.02:
+            speed -= 0.02
 
     # Detect collision with left paddle
     if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+        if speed >= 0.02:
+            speed -= 0.02
+
+    # Detect right paddle missed ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect left paddle missed ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
